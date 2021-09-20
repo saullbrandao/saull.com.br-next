1a2320e1ee828008666b36853e3dc083c6ae8879
Saull Brandão <saull@outlook.com>
2021-09-20 09:42:01 -0300

    feat: create about component

---
+++ src/components/About/index.tsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        voluptatum cum enim dolorem excepturi culpa hic, rem nulla amet dolore
+        rerum molestias doloremque repellendus labore harum temporibus repellat?
+        Voluptatem reprehenderit officia odit corporis fugiat laborum sed veniam
+        impedit? Veritatis, sint?
+      </p>
+    </div>
+  )
+}
+
+export default About
