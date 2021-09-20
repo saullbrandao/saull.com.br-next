d85b38d2972b63c2ac5c08df442290fb4ccfb18d
Saull Brandão <saull@outlook.com>
2021-09-20 09:01:31 -0300

    feat: add some default css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --black_primary: #1a1a1a;
+    --black_secondary: #333333;
+    --white_primary: #ffffff;
+    --white_secondary: #d4d4d4;
+    --highlight: #64baff;
+    --container: 100rem;
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
+
    * {
     margin: 0;
     padding: 0;
@@ -13,10 +26,21 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--black_primary);
+    color: var(--white_secondary);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 
