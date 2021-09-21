44bbad5cac74870d9eae6a79ca45e69cedde734c
Saull Brandão <saull@outlook.com>
2021-09-21 09:06:09 -0300

    feat: change the width and gap

--- src/components/Project/styles.tsx
+++ src/components/Project/styles.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: var(--small);
   align-items: center;
-  width: 60rem;
+  width: 100%;
 
   img {
     width: 100%;
