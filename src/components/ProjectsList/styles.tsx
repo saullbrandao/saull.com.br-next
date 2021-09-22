cfe8dc66ac2de0d51e2ce9a1c167bf63268fe213
Saull Brandão <saull@outlook.com>
2021-09-22 10:50:51 -0300

    feat: increase the gap between projects

--- src/components/ProjectsList/styles.tsx
+++ src/components/ProjectsList/styles.tsx
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: var(--medium);
+  gap: var(--large);
   max-width: var(--container);
   margin: 0 auto;
   padding: var(--small);
