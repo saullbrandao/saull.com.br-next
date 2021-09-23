553eeae1e8e6f933a03e6c9dc752a889d80e365f
Saull Brandão <saull@outlook.com>
2021-09-23 11:21:30 -0300

    refactor: move the font import to the _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,16 +9,6 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={darkTheme}>
       <Head>
         <title>Saull Brandão</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
