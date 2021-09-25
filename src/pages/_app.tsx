c16ba347616ae73b4c9aede902225b96d1740218
Saull Brandão <saull@outlook.com>
2021-09-25 12:00:23 -0300

    feat: save users prefered theme to localstorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ThemeContext } from 'hooks/useTheme'
 import { AppProps } from 'next/dist/shared/lib/router/router'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import { darkTheme, lightTheme } from 'styles/themes'
@@ -12,7 +12,22 @@ function App({ Component, pageProps }: AppProps) {
   const toggleTheme = () => {
     const newTheme = theme.title === 'light' ? darkTheme : lightTheme
     setTheme(newTheme)
+    localStorage.theme = newTheme.title
   }
+
+  useEffect(() => {
+    // check if the theme is stored on localStorage
+    // if not, check if the user's system preferred color scheme is set to dark
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      setTheme(darkTheme)
+    } else {
+      setTheme(lightTheme)
+    }
+  }, [])
 
   return (
     <ThemeContext.Provider value={{ themeMode: theme.title, toggleTheme }}>
