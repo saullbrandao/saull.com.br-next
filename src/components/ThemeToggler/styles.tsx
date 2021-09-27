ae98e230def47a162acaeb15b531df5f73017bb4
Saull Brandão <saull@outlook.com>
2021-09-27 09:22:43 -0300

    feat: change the themetoggler to be a button

--- src/components/ThemeToggler/styles.tsx
+++ src/components/ThemeToggler/styles.tsx
@@ -1,12 +1,24 @@
 import styled from 'styled-components'
 
-export const Toggle = styled.a`
+export const Toggle = styled.button`
   min-width: 2rem;
   height: auto;
   cursor: pointer;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  font-size: var(--small);
+  text-decoration: none;
+  line-height: var(--medium);
+  border: 0;
+  padding: 0;
+  background: inherit;
+  color: ${(props) => props.theme.colors.text_primary};
+
+  &:hover {
+    color: ${(props) => props.theme.colors.highlight};
+  }
 
   svg {
     width: 100%;
