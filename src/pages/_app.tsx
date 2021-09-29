df1c9f19b7eb2fe23c14b289fec1164832346a30
Saull Brandão <saull@outlook.com>
2021-09-29 16:23:37 -0300

    fix: use cookie instead of localstorage to persist theme preference and prevent initial flash

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
 import { ThemeContext } from 'hooks/useTheme'
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import { AppContextType } from 'next/dist/shared/lib/utils'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import nookies from 'nookies'
 import { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import { darkTheme, lightTheme } from 'styles/themes'
 
-function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState(darkTheme)
+type ThemeType = {
+  title: string
+  colors: {
+    bg_primary: string
+    bg_secondary: string
+    text_primary: string
+    text_secondary: string
+    highlight: string
+  }
+}
+
+type Props = AppProps & {
+  preferredTheme?: ThemeType
+}
+
+function App({ Component, pageProps, preferredTheme }: Props) {
+  const [theme, setTheme] = useState(preferredTheme || darkTheme)
   const { locale } = useRouter()
 
   const metaDescription =
@@ -19,20 +36,30 @@ function App({ Component, pageProps }: AppProps) {
   const toggleTheme = () => {
     const newTheme = theme.title === 'light' ? darkTheme : lightTheme
     setTheme(newTheme)
-    localStorage.theme = newTheme.title
+
+    handleTheme(newTheme)
+  }
+
+  const handleTheme = (newTheme: ThemeType) => {
+    setTheme(newTheme)
+    nookies.set(null, 'NEXT_THEME', newTheme.title, {
+      maxAge: 100 * 365 * 24 * 60 * 60,
+      path: '/'
+    })
   }
 
   useEffect(() => {
-    // check if the theme is stored on localStorage
-    // if not, check if the user's system preferred color scheme is set to dark
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      setTheme(darkTheme)
+    const themeCookieExists = nookies.get().NEXT_THEME
+    if (themeCookieExists) return
+
+    const preferDarkTheme = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches
+
+    if (preferDarkTheme) {
+      handleTheme(darkTheme)
     } else {
-      setTheme(lightTheme)
+      handleTheme(lightTheme)
     }
   }, [])
 
@@ -48,6 +75,13 @@ function App({ Component, pageProps }: AppProps) {
       </ThemeProvider>
     </ThemeContext.Provider>
   )
+}
+
+App.getInitialProps = async ({ ctx }: AppContextType) => {
+  const { NEXT_THEME } = nookies.get(ctx)
+  const preferredTheme = NEXT_THEME === 'light' ? lightTheme : darkTheme
+
+  return { preferredTheme }
 }
 
 export default App
